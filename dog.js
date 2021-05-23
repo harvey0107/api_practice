8a442e6c09d0197ba1d2afd062638097ca0499dd
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-22 22:32:33 -0400

    added url

--- dog.js
+++ dog.js
@@ -4,5 +4,9 @@ const dog_btn = document.getElementById('dog_btn');
 dog_btn.addEventListener('click', handleDogBtn)
 
 function handleDogBtn (){
-    
+    fetch('https://random.dog/woof.json')
+    .then(res => res.json())
+    .then(data => {
+        dog_result.innerHTML = `<img src="${data.url}"/>`
+    })
 }
