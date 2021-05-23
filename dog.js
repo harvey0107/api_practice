b720856a7e8f8cb9d1710bed9dd59c2469955044
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-22 22:23:14 -0400

    added function for handledogBtn

---
+++ dog.js
@@ -0,0 +1,8 @@
+const dog_result = document.getElementById('dog_result');
+const dog_btn = document.getElementById('dog_btn');
+
+dog_btn.addEventListener('click', handleDogBtn)
+
+function handleDogBtn (){
+    
+}
